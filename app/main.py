fb9eebff7466192f6d3417cee069c93eb5f97087
Rael Max <contato@raelmax.com>
2011-02-14 01:44:35 -0200

    corrigido erro em nao selecionar sexo, adicionado analytics e fix em css

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import cgi
 import os
+from random import choice
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -12,9 +13,16 @@ from app.models import Nome
 class MainPage(webapp.RequestHandler):
     path = os.path.join(TEMPLATE_FOLDER, 'index.html')
     def get(self):
-        self.response.out.write(template.render(self.path, locals()))
+        self.response.out.write(template.render(self.path, {}))
 
     def post(self):
+        if self.request.get('sexo'):
+            bichos = Nome.all()
+            bichos = bichos.filter("tipo =", self.request.get('sexo'))
+            l = []
+            for obj in bichos:
+                l.append(obj.nome)
+            bicho = choice(l)
         self.response.out.write(template.render(self.path, locals()))
 
 application = webapp.WSGIApplication(
